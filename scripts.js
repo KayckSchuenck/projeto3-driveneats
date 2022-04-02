565c76d5a9c338331101d4efd2a51efb14544c74
Kayck Schuenck <ycferreiras@gmail.com>
2022-04-02 01:48:12 -0300

    botoes finalizados

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
-function selecionar(elemento) {
-    const item=document.querySelector(".selecionado");
+function selecionarprato(elemento) {
+    const item=document.querySelector(".pselecionado");
     if (item !== null) {
-        item.classList.remove("selecionado");
+        item.classList.remove("pselecionado");
       }
-      elemento.classList.add("selecionado");
+      elemento.classList.add("pselecionado");
+}
+function selecionarbebida(elemento) {
+  const item=document.querySelector(".bselecionada");
+  if (item !== null) {
+      item.classList.remove("bselecionada");
+    }
+    elemento.classList.add("bselecionada");
+}
+function selecionarsobremesa(elemento) {
+  const item=document.querySelector(".sselecionada");
+  if (item !== null) {
+      item.classList.remove("sselecionada");
+    }
+    elemento.classList.add("sselecionada");
+}
+
+function botaofinal() {
+  if (document.querySelector(".pselecionado")!=null && document.querySelector(".bselecionada")!=null && document.querySelector(".sselecionada")!=null) 
+  {
+  document.querySelector(".incompleto").classList.add("hidden");
+  document.querySelector(".completo").classList.remove("hidden");
+  }
 }
